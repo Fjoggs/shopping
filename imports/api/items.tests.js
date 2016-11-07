096a83f3f24169ef5b0e1dea6e838114f2ada299
vegfjogs <vegard.fjogstad@finn.no>
2016-11-07 20:22:51 +0100

    Set up mocha test framework, and added delete test.

---
+++ imports/api/items.tests.js
@@ -0,0 +1,37 @@
+/* eslint-env mocha */
+
+import { Meteor } from 'meteor/meteor';
+import { Random } from 'meteor/random';
+import { assert } from 'meteor/practicalmeteor:chai';
+
+import { Items } from './items.js';
+
+if (Meteor.isServer) {
+  describe('Items', () => {
+    describe('methods', () => {
+      const userId = Random.id();
+      let itemId;
+
+      beforeEach( () => {
+        Items.remove({});
+        itemId = Items.insert( {
+          name: 'Test name',
+          amount: '100',
+          price: '50',
+          store: 'Test store',
+          owner: userId,
+          username: 'Test username',
+        });
+      });
+
+      it('can delete owned item', () => {
+        const deleteItem = Meteor.server.method_handlers['items.remove'];
+        const invocation = { userId };
+
+        deleteItem.apply(invocation, [itemId]);
+
+        assert.equal(Items.find().count(), 0);
+      });
+    });
+  });
+}
