06afa4a83cac5544d88a7b5df49587ee775cbe9c
Vegard Fjogstad <fjoggis@gmail.com>
2016-11-08 23:27:50 +0100

    Added sinon for stubbing, cleaner for cleaning database and removed hwillson stub-collections. All tests are now green! :star:

--- imports/api/items.tests.js
+++ imports/api/items.tests.js
@@ -2,46 +2,36 @@
 
 import { Meteor } from 'meteor/meteor';
 import { Random } from 'meteor/random';
+import { stubs } from 'meteor/practicalmeteor:sinon';
 import { assert, expect } from 'meteor/practicalmeteor:chai';
-import StubCollections from 'meteor/hwillson:stub-collections';
+import { resetDatabase } from 'meteor/xolvio:cleaner';
 
 import { Items } from './items.js';
 
 if (Meteor.isServer) {
-  describe('Items', () => {
+  describe('Items collection', () => {
     describe('methods', () => {
       const userId = Random.id();
       let itemId, invocation;
 
       beforeEach( () => {
-        // todo: fix stubbing
         invocation = { userId };
-        StubCollections.stub(Items);
-        itemId = Items.insert( {
-          name: 'Test name',
-          amount: '100',
-          price: '50',
-          store: 'Test store',
-          owner: userId,
-          username: 'Test username',
-          private: true,
-        });
+        resetDatabase();
       });
 
-      describe('insert', () => {
-        let insertItem;
-        beforeEach( () => {
-          Items.remove({});
-          insertItem = Meteor.server.method_handlers['items.insert'];
-        });
-
+      describe('insert item', () => {
         it('can insert if authorized', () => {
+          const insertItem = Meteor.server.method_handlers['items.insert'];
+          stubs.create('findOne', Meteor.users, 'findOne');
+          stubs.findOne.returns({ username: 'Test username'})
           insertItem.apply(invocation, ['name', '1', '1', 'store']);
 
           assert.equal(Items.find().count(), 1);
+          stubs.restoreAll();
         });
 
         it('cannot insert if not authorized', () => {
+          const insertItem = Meteor.server.method_handlers['items.insert'];
           const wrongUser = { null };
 
           expect(function() {
@@ -52,69 +42,77 @@ if (Meteor.isServer) {
         });
       });
 
-      describe('delete', () => {
-        let deleteItem;
+      describe('one item inserted', () => {
         beforeEach( () => {
-          deleteItem = Meteor.server.method_handlers['items.remove'];
-        });
-        it('can delete if owner', () => {
-          deleteItem.apply(invocation, [itemId]);
-
-          assert.equal(Items.find().count(), 0);
-        });
-
-        it('cannot delete if not owner', () => {
-          const wrongUser = { null }
-
-          expect(function() {
-            deleteItem.apply(wrongUser, [itemId]);
-          }).to.throw('not-authorized');
-
-          assert.equal(Items.find().count(), 1);
-        });
-      });
-
-      describe('check', () => {
-        let checkItem;
-        beforeEach( () => {
-          checkItem = Meteor.server.method_handlers['items.setChecked'];
-        })
-        it('can check if owner', () => {
-          checkItem.apply(invocation, [itemId, true]);
-
-          assert.equal(Items.find({ checked: true }).count(), 1);
+          itemId = Items.insert( {
+            name: 'Test name',
+            amount: '100',
+            price: '50',
+            store: 'Test store',
+            owner: userId,
+            username: 'Test username',
+            private: true,
+          });
         });
 
-        it('cannot check if not owner and private', () => {
-          const wrongUser = { null }
+        describe('delete item', () => {
+          it('can delete if owner', () => {
+            const deleteItem = Meteor.server.method_handlers['items.remove'];
+            deleteItem.apply(invocation, [itemId]);
 
-          expect(function() {
-            checkItem.apply(wrongUser, [itemId,true]);
-          }).to.throw('not-authorized');
+            assert.equal(Items.find().count(), 0);
+          });
 
-          assert.equal(Items.find({ checked: true }).count(), 0);
-        });
-      });
+          it('cannot delete if not owner', () => {
+            const deleteItem = Meteor.server.method_handlers['items.remove'];
+            const wrongUser = { null }
 
-      describe('private status', () => {
-        let setPrivate;
-        beforeEach( () => {
-          setPrivate = Meteor.server.method_handlers['items.setPrivate'];
-        })
-        it('can change status if owner', () => {
-          setPrivate.apply(invocation, [itemId, false]);
+            expect(function() {
+              deleteItem.apply(wrongUser, [itemId]);
+            }).to.throw('not-authorized');
 
-          assert.equal(Items.find({ private: false }).count(), 1);
+            assert.equal(Items.find().count(), 1);
+          });
         });
 
-        it('cannot change status if not owner', () => {
-          const wrongUser = { null }
+        describe('check item', () => {
+          it('can check if owner', () => {
+            const checkItem = Meteor.server.method_handlers['items.setChecked'];
+            checkItem.apply(invocation, [itemId, true]);
 
-          expect(function() {
-            setPrivate.apply(wrongUser, [itemId, false]);
-          }).to.throw('not-authorized');
+            assert.equal(Items.find({ checked: true }).count(), 1);
+          });
 
-          assert.equal(Items.find({ private: true }).count(), 1);
+          it('cannot check if not owner and private', () => {
+            const checkItem = Meteor.server.method_handlers['items.setChecked'];
+            const wrongUser = { null }
+
+            expect(function() {
+              checkItem.apply(wrongUser, [itemId,true]);
+            }).to.throw('not-authorized');
+
+            assert.equal(Items.find({ checked: true }).count(), 0);
+          });
+        });
+
+        describe('private item', () => {
+          it('can change status if owner', () => {
+            const setPrivate = Meteor.server.method_handlers['items.setPrivate'];
+            setPrivate.apply(invocation, [itemId, false]);
+
+            assert.equal(Items.find({ private: false }).count(), 1);
+          });
+
+          it('cannot change status if not owner', () => {
+            const setPrivate = Meteor.server.method_handlers['items.setPrivate'];
+            const wrongUser = { null }
+
+            expect(function() {
+              setPrivate.apply(wrongUser, [itemId, false]);
+            }).to.throw('not-authorized');
+
+            assert.equal(Items.find({ private: true }).count(), 1);
+          });
         });
       });
     });
