0a63ec4c953c865812c2532e24da3f5128eb4efd
vegfjogs <vegard.fjogstad@finn.no>
2016-11-29 14:12:06 +0100

    Linting rules to prevent unsued React variables crying.

--- .eslintrc.js
+++ .eslintrc.js
@@ -31,6 +31,9 @@ module.exports = {
         "semi": [
             "error",
             "always"
-        ]
+        ],
+        "react/jsx-uses-react": 1,
+        "react/jsx-uses-vars": 1,
+        "react/react-in-jsx-scope": 1
     }
 };
